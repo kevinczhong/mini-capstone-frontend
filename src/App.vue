21af949b34b65961959ce7e6b9db1ac2d9445d2d
Kevin Zhong <kvnzhong@gmail.com>
2022-06-09 11:46:58 -0700

    Successfully implemented navbar; search function remains non-functional.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,60 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Storefront</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link active" to="/">Home</router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Products
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><router-link class="dropdown-item" to="/products">All Products</router-link></li>
+              <li><router-link class="dropdown-item" to="/products/new">New Product</router-link></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/signup">Signup</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/logout">Logout</router-link>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  <router-view class="container" />
+</template>
+
+<!-- <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light container">
     <a class="navbar-brand" href="#">Storefront</a>
     <button
@@ -52,7 +108,7 @@
     <router-link to="/logout">Logout</router-link>
   </nav>
   <router-view />
-</template>
+</template> -->
 
 <style>
 #app {
